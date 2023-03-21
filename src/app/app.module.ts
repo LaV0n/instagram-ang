13d56adf07df7bc8046f64f36ddbf75768e97138
LaV0n <leanid.dev@gmail.com>
2023-03-21 14:34:57 +0100

    feat: part 12, refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { AppComponent } from './app.component'
 import { FormsModule } from '@angular/forms'
 import { ParentComponent } from './parent/parent.component'
 import { ChildComponent } from './parent/child/child.component'
+import { TodosComponent } from './todos/todos.component'
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
-  declarations: [AppComponent, ParentComponent, ChildComponent],
-  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, ParentComponent, ChildComponent, TodosComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
