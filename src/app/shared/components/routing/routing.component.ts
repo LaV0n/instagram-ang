2e4e21648d70d14c6a87cda6e33feeb2ab4a11f0
LaV0n <leanid.dev@gmail.com>
2023-04-04 10:16:54 +0200

    feat: add routing

---
+++ src/app/shared/components/routing/routing.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'inst-routing',
+  templateUrl: './routing.component.html',
+  styleUrls: ['./routing.component.css'],
+})
+export class RoutingComponent {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { RoutingComponent } from './components/routing/routing.component'
+import { RouterModule } from '@angular/router'
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule],
+  declarations: [RoutingComponent],
+  imports: [CommonModule, RouterModule],
+  exports: [RoutingComponent],
 })
 export class SharedModule {}
